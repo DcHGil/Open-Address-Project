5b0f579975c59986b7189b9fdf38c313f574bac6
Gil linuxD <swk609@gmail.com>
2025-10-15 22:48:38 -0600

    feat(data): add municipios for Hidalgo, Jalisco, Estado de Mexico, update master insert file

--- sql/estados_unidos_mexicanos/data/insert__estados_municipios.sql
+++ sql/estados_unidos_mexicanos/data/insert__estados_municipios.sql
@@ -11,3 +11,9 @@
 \i insert_municipios_chiapas.sql
 \i insert_municipios_chihuahua.sql
 \i insert_municipios_ciudad_de_mexico.sql
+\i insert_municipios_durango.sql
+\i insert_municipios_guanajuato.sql
+\i insert_municipios_guerrero.sql
+\i insert_municipios_hidalgo.sql
+\i insert_municipios_jalisco.sql
+\i insert_municipios_mexico.sql
